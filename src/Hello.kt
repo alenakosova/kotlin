6e1806c2fcc74969570cf5b3eff801bf033f926b
dead4x <a.kosova94@gmail.com>
2020-06-23 00:13:23 +0300

    Задание 3

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>){
-    var hello = "hello"
-    var aplana = "aplana"
-    println("$hello $aplana")
+    var five: Char = '5'
+    five.toString()
+    five.toByte()
+    five.toShort()
+    five.toInt()
+    five.toDouble()
+    five.toFloat()
+    five.toLong()
 }
 

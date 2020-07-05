bcc5389925cfc5f5bacb1858d1d03299ba922046
alena.kosova <a.kosova94@gmail.com>
2020-07-05 21:24:58 +0300

    Задание 14: Инфо сотрудника

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,7 +1,22 @@
 fun main() {
-    println("Введено ${getEmployeesCount("Вася", "Петя", "Маша", "Оля")} имен сотрудников")
+    getEmployeeInfo("Лаптев Николай", 55, "Не женат", "Таксист")
 }
 
-fun getEmployeesCount(vararg employees:String) = employees.count()
+fun getEmployeeInfo(name:String, position:String) {
+    println("ФИО сотрудника: $name")
+    println("Должность: $position")
+}
+fun getEmployeeInfo(name:String, age:Int, position: String){
+    getEmployeeInfo(name, position)
+    println("Возраст: $age")
+}
 
+fun getEmployeeInfo(name:String, family:String, position: String){
+    getEmployeeInfo(name, position)
+    println("Семейное положение: $family")
+}
 
+fun getEmployeeInfo(name:String, age:Int, family:String, position: String){
+    getEmployeeInfo(name, age, position)
+    println("Семейное положение: $family")
+}

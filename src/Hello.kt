401201cd15b0a553fd8eadf97fc58552511e0ec8
alena.kosova <a.kosova94@gmail.com>
2020-07-06 19:43:56 +0300

    Задание 17: Авиакомпания 1

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,4 +1,31 @@
-fun main() = 12.00.sqrt()
-fun Double.sqrt(){
-    print(kotlin.math.sqrt(this))
+fun main(){
+    val somePlane = Aircraft()
+    somePlane.planeNumber = "001"
+    somePlane.maxRange = 20000
+    somePlane.tankCapacity = 10000
+    print(somePlane.fuelPerKm)
+}
+
+class Aircraft {
+    var planeNumber = "1"
+    var maxRange = 10000
+        set(value) {
+            if (maxRange > 0)
+            field = value
+        }
+
+    var tankCapacity = 5000
+        set(value) {
+            if (tankCapacity > 0)
+            field = value
+        }
+
+    var fuelPerKm = 4
+        get() {
+            return maxRange/tankCapacity
+        }
+
+
+
+
 }

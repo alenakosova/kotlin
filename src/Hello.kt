15f0c4bdafb2251b2ae614fbe2cdfd7c6bbd02a5
alena.kosova <a.kosova94@gmail.com>
2020-07-06 20:02:24 +0300

    Задание 18: Авиакомпания 2. Конструктор

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,31 +1,11 @@
 fun main(){
-    val somePlane = Aircraft()
-    somePlane.planeNumber = "001"
-    somePlane.maxRange = 20000
-    somePlane.tankCapacity = 10000
+    val somePlane = Aircraft("001", 20000, 10000)
     print(somePlane.fuelPerKm)
 }
 
-class Aircraft {
-    var planeNumber = "1"
-    var maxRange = 10000
-        set(value) {
-            if (maxRange > 0)
-            field = value
-        }
-
-    var tankCapacity = 5000
-        set(value) {
-            if (tankCapacity > 0)
-            field = value
-        }
-
+class Aircraft (var planeNumber: String, var maxRange: Int, var tankCapacity: Int){
     var fuelPerKm = 4
         get() {
             return maxRange/tankCapacity
         }
-
-
-
-
 }

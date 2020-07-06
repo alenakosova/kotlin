7dcd7ad022d4fd5d90b7fccb4a1089a9645b1f7e
alena.kosova <a.kosova94@gmail.com>
2020-07-06 21:16:03 +0300

    Задание 22: Авиакомпания 6. Вывод информации

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,22 +1,32 @@
-fun main(){
-Boeing747("001", 20000, 10000, 25)
+fun main() {
+    Boeing747("001", 20000, 10000, 25)
 }
 
-abstract class Aircraft (var planeNumber: String, var maxRange: Int, var tankCapacity: Int){
-    var fuelPerKm = 4
+abstract class Aircraft(
+    protected var planeNumber: String,
+    protected var maxRange: Int,
+    protected var tankCapacity: Int
+) {
+    protected var fuelPerKm = 4
         get() {
-            return maxRange/tankCapacity
+            return maxRange / tankCapacity
         }
+
+    protected abstract fun printReference()
 }
 
-class Boeing747(planeNumber:String, maxRange:Int, tankCapacity:Int, countPassengers:Int) : Aircraft(planeNumber, maxRange, tankCapacity), Passenger{
-    override val passengers:Int
-    init{
-        passengers = countPassengers
+class Boeing747(planeNumber: String, maxRange: Int, tankCapacity: Int, countPassengers: Int) :
+    Aircraft(planeNumber, maxRange, tankCapacity), Passenger {
+    override val passengers: Int = countPassengers
+
+    override fun printReference() {
+        println("Номер самолета: $planeNumber")
+        println("Максимальная дальность полета: $maxRange")
+        println("Вместимость бака: $tankCapacity")
     }
 }
 
 interface Passenger {
-    val passengers:Int
+    val passengers: Int
 }
 

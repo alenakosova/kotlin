a36f193c4dabcc5d0ab00358142c2296d97dcb5b
alena.kosova <a.kosova94@gmail.com>
2020-07-06 18:52:33 +0300

    Задание 15: Лямбда

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,22 +1,3 @@
 fun main() {
-    getEmployeeInfo("Лаптев Николай", 55, "Не женат", "Таксист")
-}
-
-fun getEmployeeInfo(name:String, position:String) {
-    println("ФИО сотрудника: $name")
-    println("Должность: $position")
-}
-fun getEmployeeInfo(name:String, age:Int, position: String){
-    getEmployeeInfo(name, position)
-    println("Возраст: $age")
-}
-
-fun getEmployeeInfo(name:String, family:String, position: String){
-    getEmployeeInfo(name, position)
-    println("Семейное положение: $family")
-}
-
-fun getEmployeeInfo(name:String, age:Int, family:String, position: String){
-    getEmployeeInfo(name, age, position)
-    println("Семейное положение: $family")
+var a = {n:Array<String> -> n.forEach {item -> println(item)}}
 }

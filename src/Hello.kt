fad9d0094d113458b8affb54d861f030ba32a957
alena.kosova <a.kosova94@gmail.com>
2020-07-08 19:21:09 +0300

    Задание 23: Data class

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,32 +1,9 @@
 fun main() {
-    Boeing747("001", 20000, 10000, 25)
+    println(Auto("SomeBrand", "Черный", 123))
 }
 
-abstract class Aircraft(
-    protected var planeNumber: String,
-    protected var maxRange: Int,
-    protected var tankCapacity: Int
-) {
-    protected var fuelPerKm = 4
-        get() {
-            return maxRange / tankCapacity
-        }
-
-    protected abstract fun printReference()
-}
-
-class Boeing747(planeNumber: String, maxRange: Int, tankCapacity: Int, countPassengers: Int) :
-    Aircraft(planeNumber, maxRange, tankCapacity), Passenger {
-    override val passengers: Int = countPassengers
-
-    override fun printReference() {
-        println("Номер самолета: $planeNumber")
-        println("Максимальная дальность полета: $maxRange")
-        println("Вместимость бака: $tankCapacity")
+data class Auto(val brand: String, var color: String, var number: Int) {
+    override fun toString(): String {
+        return " Марка: $brand\n Цвет: $color\n Номер: $number"
     }
 }
-
-interface Passenger {
-    val passengers: Int
-}
-
